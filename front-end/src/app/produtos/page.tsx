d9215bf5107b772577ecdede3b154a654f78b251
Carlos <carlosfelipe.st28@gmail.com>
2023-08-10 11:02:48 -0400

    listando produtos

--- front-end/src/app/produtos/page.tsx
+++ front-end/src/app/produtos/page.tsx
@@ -1,23 +1,40 @@
 "use client";
 
 import FetchProdutos from "@/fetch/produtos";
+import Produtos from "@/types/Produtos";
 import { useEffect, useState } from "react";
 
 export default function TelaProdutos() {
 	const fProdutos = new FetchProdutos();
 
+	const [produtos, setProdutos] = useState<Produtos[]>([])
+
 	const getProdutos = async () => {
 		const produtos = await fProdutos.getProdutos();
+
+		setProdutos(produtos)
 	};
 
 	useEffect(() => {
 		getProdutos();
 	}, []);
+
 	return (
 		<>
-			<div>
-
-			</div>
+			<ul>
+				{produtos.map((produto) => (
+					<li>
+						<div>
+							<span>{produto.codigo}</span>
+							<span>{produto.nome}</span>
+						</div>
+						<div>
+							<span>{produto.preco}</span>
+							<span>{produto.tipo_unidade}</span>
+						</div>
+					</li>
+				))}
+			</ul>
 		</>
 	);
 }
