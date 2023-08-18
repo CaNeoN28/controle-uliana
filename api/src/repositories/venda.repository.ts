8e2cdf9b80c4b9770c73909ce0b0c1073ce662f8
Carlos <carlosfelipe.st28@gmail.com>
2023-08-18 10:51:04 -0400

    criando repositório para venda

---
+++ api/src/repositories/venda.repository.ts
@@ -0,0 +1,8 @@
+import VendaModel from "../models/Venda"
+import Venda from "../types/Venda"
+
+export default class VendaRepository {
+	static createVenda = async function(vendaData: Venda) {
+		const venda = await VendaModel.create(vendaData)
+	}
+}
