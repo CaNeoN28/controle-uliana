6a3eb29117d81f02738aca8b4ac5f153894ef43d
Carlos <carlosfelipe.st28@gmail.com>
2023-08-18 11:36:30 -0400

    calculando o total da compra

--- api/src/services/create.venda.service.ts
+++ api/src/services/create.venda.service.ts
@@ -1,8 +1,31 @@
 import VendaRepository from "../repositories/venda.repository";
 import Venda from "../types/Venda";
 
-export default async function CreateVenda(vendaData: Venda){
-	const venda = await VendaRepository.createVenda(vendaData)
+export default async function CreateVenda(vendaData: Venda) {
+	const { relacao_produtos, valor_pago, cliente } = vendaData;
 
-	return venda
+	relacao_produtos.map((produto) => {
+		const total = produto.quantidade * produto.valor
+
+		produto.total	 = total
+	})
+
+	const total = relacao_produtos.reduce((prev, curr, _i) => {
+		return {
+			...prev,
+			total: prev.total + curr.total
+		}
+	}).total
+
+	const troco = Number((valor_pago - total).toFixed(2))
+
+	const venda = await VendaRepository.createVenda({
+		relacao_produtos,
+		valor_pago,
+		cliente,
+		total,
+		troco,
+	});
+
+	return venda;
 }
