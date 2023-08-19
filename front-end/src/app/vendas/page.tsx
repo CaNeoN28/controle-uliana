b886567a7abd48b5323ac7bc889560aca2cec505
Carlos <carlosfelipe.st28@gmail.com>
2023-08-19 09:02:26 -0400

    listando produtos com base no input

--- front-end/src/app/vendas/page.tsx
+++ front-end/src/app/vendas/page.tsx
@@ -1,23 +1,35 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import styles from "./vendas.module.css";
 import Form from "@/components/Form";
 import { Controller, useForm } from "react-hook-form";
 import Venda from "@/types/Vendas";
 import TextInput from "@/components/TextInput";
 import Button from "@/components/Button";
+import Produto from "@/types/Produtos";
+import FetchProdutos from "@/fetch/produtos";
 
 export default function TelaVendas() {
-	const { handleSubmit, control } = useForm<Venda>({
+	const { handleSubmit, control, watch, reset } = useForm<Venda>({
 		defaultValues: {
-			cliente: ""
-		}
+			cliente: "",
+		},
 	});
+
+	const fProdutos = new FetchProdutos()
+
+	const [produtos, setProdutos] = useState<Produto[]>([])
 
 	const cadastrarVenda = (data: Venda) => {
 		console.log(data);
 	};
+
+	const findProdutos = async (produto: string) => {
+		const produtos = await fProdutos.getProdutos({nome: produto})
+
+		setProdutos(produtos.data)
+	} 
 
 	return (
 		<main>
@@ -35,7 +47,19 @@ export default function TelaVendas() {
 					)}
 				/>
 
-				<Button text="Cadastrar venda" type="submit"/>
+				<div>
+					<TextInput id="produtos_venda" label="Produto" onChange={(e) => {findProdutos(e.target.value)}} />
+
+					<div>
+						{produtos.length > 0 && produtos.map((produto, index) => {
+							return <div key={index}>
+								{produto.nome}
+							</div>
+						})}
+					</div>
+				</div>
+
+				<Button text="Cadastrar venda" type="submit" />
 			</Form>
 		</main>
 	);
