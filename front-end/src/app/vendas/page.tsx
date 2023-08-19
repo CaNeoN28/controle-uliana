9af95a9a311e19067d551e8fc362703bb2cbccb8
Carlos <carlosfelipe.st28@gmail.com>
2023-08-19 09:11:42 -0400

    filtrando também pelo código do produto

--- front-end/src/app/vendas/page.tsx
+++ front-end/src/app/vendas/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./vendas.module.css";
 import Form from "@/components/Form";
 import { Controller, useForm } from "react-hook-form";
@@ -11,25 +11,30 @@ import Produto from "@/types/Produtos";
 import FetchProdutos from "@/fetch/produtos";
 
 export default function TelaVendas() {
-	const { handleSubmit, control, watch, reset } = useForm<Venda>({
+	const { handleSubmit, control, watch, reset, setValue } = useForm<Venda>({
 		defaultValues: {
 			cliente: "",
 		},
 	});
 
-	const fProdutos = new FetchProdutos()
+	const fProdutos = new FetchProdutos();
 
-	const [produtos, setProdutos] = useState<Produto[]>([])
+	const [produtos, setProdutos] = useState<Produto[]>([]);
 
 	const cadastrarVenda = (data: Venda) => {
 		console.log(data);
 	};
 
 	const findProdutos = async (produto: string) => {
-		const produtos = await fProdutos.getProdutos({nome: produto})
+		let response = await fProdutos.getProdutos({ nome: produto });
 
-		setProdutos(produtos.data)
-	} 
+		if (response.data.length == 0)
+			response = await fProdutos.getProdutos({ codigo: produto });
+
+		const produtos = response.data;
+
+		setProdutos(produtos);
+	};
 
 	return (
 		<main>
@@ -47,16 +52,26 @@ export default function TelaVendas() {
 					)}
 				/>
 
-				<div>
-					<TextInput id="produtos_venda" label="Produto" onChange={(e) => {findProdutos(e.target.value)}} />
+				<div className={styles.procurarProdutos}>
+					<TextInput
+						id="produtos_venda"
+						label="Produto"
+						onChange={(e) => {
+							findProdutos(e.target.value);
+						}}
+					/>
 
-					<div>
-						{produtos.length > 0 && produtos.map((produto, index) => {
-							return <div key={index}>
-								{produto.nome}
-							</div>
-						})}
-					</div>
+					{produtos.length > 0 && (
+						<div className={styles.produtos}>
+							{produtos.map((produto, index) => {
+								return (
+									<div className={styles.produto} key={index}>
+										{produto.nome}
+									</div>
+								);
+							})}
+						</div>
+					)}
 				</div>
 
 				<Button text="Cadastrar venda" type="submit" />
