cfb881fd9c68506f768b93822e797922acd8c2a2
Carlos <carlosfelipe.st28@gmail.com>
2023-08-26 08:49:45 -0400

    formatando data final

--- api/src/services/generate.relatorio.service.ts
+++ api/src/services/generate.relatorio.service.ts
@@ -1,14 +1,33 @@
 import RepositoryRelatorio from "../repositories/relatorio.repository";
 import FiltrosRelatorio from "../types/FiltrosRelatorio";
 
-export default async function GenerateRelatorio({data_final, data_inicial}: FiltrosRelatorio){
-	if(data_inicial && isNaN(Date.parse(data_inicial)))
-		throw new Error("Data inicial inválida")
+export default async function GenerateRelatorio({
+	data_final,
+	data_inicial,
+}: FiltrosRelatorio) {
+	let {
+		data_inicial: fDataInicial,
+		data_final: fDataFinal,
+	}: FiltrosRelatorio = { data_final: "", data_inicial: "" };
 
-	if(data_final && isNaN(Date.parse(data_final)))
-		throw new Error("Data final inválida")
+	if (data_inicial) {
+		if (isNaN(Date.parse(data_inicial)))
+			throw new Error("Data inicial inválida");
 
-	const relatorio = await RepositoryRelatorio.getRelatorio({data_final, data_inicial})
+		fDataInicial = new Date(data_inicial).toString() 
+	}
 
-	return relatorio
+	if (data_final){
+		if(isNaN(Date.parse(data_final)))
+		throw new Error("Data final inválida");
+
+		fDataFinal = new Date(data_final).setHours(23, 59, 59).toString()
+	}
+
+	const relatorio = await RepositoryRelatorio.getRelatorio({
+		data_final: fDataFinal,
+		data_inicial: fDataInicial,
+	});
+
+	return relatorio;
 }
