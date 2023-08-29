faf550b34807a63c8c113c00b65e798a909f088d
Carlos <carlosfelipe.st28@gmail.com>
2023-08-29 10:51:44 -0400

    estilizando a página

--- front-end/src/app/relatorios/page.tsx
+++ front-end/src/app/relatorios/page.tsx
@@ -3,8 +3,10 @@
 import Input from "@/components/Input";
 import Return from "@/components/Return";
 import { useState } from "react";
-import "../../styles/variables.css";
 import Button from "@/components/Button";
+import styles from "./relatorios.module.css";
+
+import "../../styles/variables.css";
 
 export default function Relatorios() {
 	const [dataInicial, setDataInicial] = useState("");
@@ -13,31 +15,33 @@ export default function Relatorios() {
 	return (
 		<main>
 			<Return to="/">Voltar</Return>
-			<div>
-				<Input
-					id="data_inicial"
-					type="date"
-					label="Data inicial"
-					value={dataInicial}
-					onChange={(e) => setDataInicial(e.target.value)}
-				/>
-				<Input
-					id="data_final"
-					type="date"
-					label="Data final"
-					value={dataFinal}
-					onChange={(e) => setDataFinal(e.target.value)}
-				/>
+			<div className={styles.relatorios}>
+				<div className={styles.inputs}>
+					<Input
+						id="data_inicial"
+						type="date"
+						label="Data inicial"
+						value={dataInicial}
+						onChange={(e) => setDataInicial(e.target.value)}
+					/>
+					<Input
+						id="data_final"
+						type="date"
+						label="Data final"
+						value={dataFinal}
+						onChange={(e) => setDataFinal(e.target.value)}
+					/>
+				</div>
 				<Button
 					disabled={!dataInicial || !dataFinal}
-					text="Gerar Relatório"
+					text="Relatório"
 					onClick={(e) => {
 						e.preventDefault();
 
 						console.log({
 							dataInicial,
-							dataFinal
-						})
+							dataFinal,
+						});
 					}}
 				/>
 			</div>
